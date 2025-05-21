8f3bee84e1f1e151127863057e0c6ea399517348
Richmond Addai <richmondaddai46@gmail.com>
2025-05-21 12:00:08 +0000

    Add frontend code for fertilizer predictor

--- frontend/src/components/FertilizerPrediction.jsx
+++ frontend/src/components/FertilizerPrediction.jsx
@@ -23,7 +23,7 @@ function FertilizerPrediction() {
     setPrediction(null);
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://fertilizer-predictor-project.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
